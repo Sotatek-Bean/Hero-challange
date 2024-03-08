94908ae21ca28066ecc634e9c569d9fbe903b43a
Sotatek-Bean <bean.kai29@gmail.com>
2024-03-08 20:32:41 +0700

    fix: add unsub on destroy

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 
 import { Hero } from '../../models/common-models';
 import { EntityService } from '../../services/entity.service';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from '../components/hero-detail/hero-detail.component';
 import { InventoryComponent } from "../components/inventory/inventory.component";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-heroes',
@@ -16,14 +17,21 @@ import { InventoryComponent } from "../components/inventory/inventory.component"
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   currentViewHero: Hero | undefined;
+  private unsub$ = new Subject<void>();
+
   private entityService = inject(EntityService);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
+    this.destroyRef.onDestroy(() => {
+      this.unsub$.next();
+      this.unsub$.complete();
+    });
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.entityService.getHeroes()
+    this.entityService.getHeroes().pipe(takeUntil(this.unsub$))
     .subscribe(heroes => {
       this.heroes = heroes;
       if (heroes.length) {

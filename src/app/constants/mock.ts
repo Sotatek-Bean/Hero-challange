2d80423b1b7d08ce274deff137199e468302a756
Sotatek-Bean <bean.kai29@gmail.com>
2024-03-08 22:15:36 +0700

    chore: add comments for models

--- src/app/constants/mock.ts
+++ src/app/constants/mock.ts
@@ -25,7 +25,7 @@ export const MONSTER: Monster[] = [
   { id: 3, name: 'Berserker', avatar: 'assets/m3.png', type: EntityType.monster, level: 1, atk: 25, health: 125, speed: 0},
   { id: 4, name: 'The Demon', avatar: 'assets/boss.png', type: EntityType.monster, level: 1, atk: 25, health: 200, speed: 15},
 ];
-
+// group name and background for randomize
 export const WEAPON_TYPES: Identity[] = [
   { name: 'Red Dagger', avatar: 'assets/w1.jpg'},
   { name: 'Titanium Axe', avatar: 'assets/w2.jpg', },
@@ -33,13 +33,14 @@ export const WEAPON_TYPES: Identity[] = [
   { name: 'Ice Gun', avatar: 'assets/w4.jpg', },
   { name: 'M.Rifle', avatar: 'assets/w5.jpg', },
 ];
-
+// group name and background for randomize
 export const ARMOR_TYPES: Identity[] = [
   { name: 'Iron Shield', avatar: 'assets/am3.jpg', },
   { name: 'Iron Armor', avatar: 'assets/am1.jpg', },
   { name: 'Helmet', avatar: 'assets/am2.jpg', },
 ];
 
+// default data init when first play or reset
 export const DEFAULT_SAVE: SaveData = {
   heroes: HEROES,
   items: BASE_ITEMS,
--- src/app/models/common-models.ts
+++ src/app/models/common-models.ts
@@ -1,9 +1,8 @@
-import { DestroyRef, inject } from "@angular/core";
-
 export interface Hero extends Entity {
   cost: number;
   unlocked: boolean;
   type: EntityType.hero;
+  // equip Records for items (can equip unlimited of item type as please in future)
   equip: Record<EntityType.weapon | EntityType.armor, number | undefined>;
   atk: number;
   speed: number;
@@ -17,10 +16,12 @@ export interface Monster extends Entity {
   health: number;
 }
 
+// items/equiments affect stats
 export interface Item extends Entity {
   type: EntityType.weapon | EntityType.armor;
 }
 
+// entity invole in fighting included items
 export interface Entity extends Stats, Identity {
   maxHp?: number;
   id: number;
@@ -28,10 +29,13 @@ export interface Entity extends Stats, Identity {
   type: EntityType;
 }
 
+// for showing visually
 export interface Identity {
   name: string;
   avatar?: string;
 }
+
+// stats for fighting
 export interface Stats {
   atk?: number;
   speed?: number;
